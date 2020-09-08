6b8f2caff019a14025d68557c163594dcc482b33
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-09-08 12:59:30 +0530

    Integrate the design for drag and drop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { DragDropContainer } from 'react-drag-drop-container';
+import DragDropContainer from './DragDropContainer';
+import DropTarget from './DropTarget';
+import './App.css';
 
 class App extends Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+        this.state =  {
+            draggableImages : [],
+            currentSelectedIndex : 0,
+            isDragged : false
+        }
+    }
+
+    componentDidMount (){
         const context = require.context('./images', true, /.png$/);
         const imagesNameCollection = [];
-         context.keys().forEach((key) => {
-            imagesNameCollection.push(key.replace("./",""));
+        context.keys().forEach((data, key) => {
+            imagesNameCollection.push({ index : key, imageName : data.replace("./","") , isDragged : false })
+        });   
+        this.setState({
+            draggableImages : imagesNameCollection
         });
+    }
+
+    onDragStart = (index) => {
+        this.setState({
+            currentSelectedIndex : index,
+            isDragged : false
+        });
+    };
+
+    onDragEnd = () => {
+        let updatedDrggableImages = this.state.draggableImages;
+        updatedDrggableImages[this.state.currentSelectedIndex].isDragged = this.state.isDragged;
+        this.setState({
+            draggableImages : updatedDrggableImages,
+            isDragged : false
+        });    
+    }
+
+    onHit = () =>{
+        this.setState({
+            isDragged : true
+        });
+    }
+
+    onDragLeave = () => {
+        this.setState({
+            isDragged : false
+        });
+    }
+
+    render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12">
-                        <h3>Draggable / Droppable Example</h3>
+                        <h3>Draggable / Droppable Example</h3><br />
                     </div>
-                    <div className="col-sm-6" style={{backgroundColor : 'lightyellow', minHeight : "500px"}}>
+                    <div className="col-sm-6 leftContainer">
+                        <div className="row p-2 draggbleContainer">
+                            <label className="defaultLable">Drag Me here!</label>
+                            <DropTarget
+                                onDragEnter={() => (console.log('onDragEnter'))}
+                                onDragLeave={this.onDragLeave}
+                                onHit={this.onHit}
+                                targetKey="simple">
+                            </DropTarget>
+                            {this.state.draggableImages.map((data, key) => {
+                                if (data.isDragged) {
+                                    return <DragDropContainer key={key}
+                                        onDragStart={() => this.onDragStart(data.index)}
+                                        onDrag={() => (console.log('dragging'))}
+                                        onDragEnd={this.onDragEnd}
+                                        targetKey="simple"
+                                        className="col-sm-2">
+                                        <img width="78px" src={require("./images/" + data.imageName)} />
+                                    </DragDropContainer>
+                                }
+                            })}
+                        </div>
                     </div>
-                    <div className="col-sm-6" style={{ backgroundColor: 'lightgray', minHeight: "500px" }}>
+                    <div className="col-sm-6" style={{ backgroundColor: 'lightgray', minHeight: "500px", border: "1px solid black" }}>
                         <div className="row p-2">
-                            {imagesNameCollection.map((data, key) => {
-                                return <DragDropContainer className="col-sm-2"><img width="78px" src={require("./images/" + data)} /></DragDropContainer>
+                            {this.state.draggableImages.map((data, key) => {
+                                if (!data.isDragged) {
+                                    return <DragDropContainer key={key}
+                                        onDragStart={() => this.onDragStart(data.index)}
+                                        onDrag={() => (console.log('dragging'))}
+                                        onDragEnd={this.onDragEnd}
+                                        targetKey="simple"
+                                        className="col-sm-2">
+                                        <img width="78px" src={require("./images/" + data.imageName)} />
+                                    </DragDropContainer>
+                                }
                             })}
                         </div>
                     </div>
